65719a2f72d8eab2721b1f545bec236971e20b7b
samielmejri <sami.elmejri@esprit.tn>
2023-07-21 14:00:29 +0100

    Local Storage

--- test1/src/components/TodoForm.js
+++ test1/src/components/TodoForm.js
@@ -1,22 +1,45 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react';
 
-export const TodoForm = ({addTodo}) => {
-    const [value, setValue] = useState('');
+export const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState('');
 
-    const handleSubmit = (e) => {
-      // prevent default action
-        e.preventDefault();
-        if (value) {
-          // add todo
-          addTodo(value);
-          // clear form after submission
-          setValue('');
-        }
-      };
+  // Load tasks from local storage when the component mounts
+  useEffect(() => {
+    const savedTasks = localStorage.getItem('tasks');
+    if (savedTasks) {
+      const parsedTasks = JSON.parse(savedTasks);
+      addTodo(parsedTasks);
+    }
+  }, []); // Empty dependency array to run only once on mount
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (value) {
+      // add todo
+      addTodo(value);
+      // clear form after submission
+      setValue('');
+
+      // Save tasks to local storage
+      const savedTasks = localStorage.getItem('tasks');
+      const tasks = savedTasks ? JSON.parse(savedTasks) : [];
+      tasks.push(value);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+  };
+
   return (
     <form onSubmit={handleSubmit} className="TodoForm">
-    <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='What is the task today?' />
-    <button type="submit" className='todo-btn'>Add Task</button>
-  </form>
-  )
-}
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className="todo-input"
+        placeholder="What is the task today?"
+      />
+      <button type="submit" className="todo-btn">
+        Add Task
+      </button>
+    </form>
+  );
+};
